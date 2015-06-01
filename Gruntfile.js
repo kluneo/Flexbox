4b95d0271f10a9f9df0cf1b2c56a01d987a81197
Maquettage <valentin.malherbe@priceminister.com>
2015-06-01 19:07:29 +0200

    Add reviews Modify navigation style

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,6 +47,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
   
 
 };
